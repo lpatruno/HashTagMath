090d466a02dc65f5ed7343aaf99496a84012b623
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-16 16:10:37 -0400

    edit

--- static/functions.js
+++ static/functions.js
@@ -3,6 +3,9 @@ $( document ).ready(function() {
 	$.get('/getQuestion',function(data){
 		var question = data['question'];
 		var keywords = data['keywords'];
+		
+		$('#questionText').html(question);
+		
 		console.log(question);
 		console.log(keywords);
 	});
