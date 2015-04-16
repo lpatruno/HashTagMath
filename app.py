1ac75c9d52705ad6b8d519c23356f6f17cf545c6
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-16 15:02:17 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+    
+@app.route('/model')
+def model_test():
+    return None
+    
+    
+@app.route('/user/<name>')
+def test_variables(name):
+    return "String: %s" % name
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
