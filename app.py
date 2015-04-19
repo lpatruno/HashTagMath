5c35f54d2c168548c75b0ea6f9a48c0d8d289d04
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-19 16:00:14 -0400

    javascript about page

--- app.py
+++ app.py
@@ -7,8 +7,36 @@ app = Flask(__name__)
 
 # Global variable containing the question information
 #question_info = pd.read_csv('/home/vagrant/datacourse/MathQuestionTagging/data/question_info_data_2.csv', index_col=0)
+tag_df = pd.read_csv('/home/vagrant/datacourse/MathQuestionTagging/data/tag_info_data_2.csv', index_col=0)
 latex_text_40_df = pd.read_csv('/home/vagrant/datacourse/MathQuestionTagging/data/latex_text_40_results.csv', index_col=0)
 #latex_text_50_df = pd.read_csv('/home/vagrant/datacourse/MathQuestionTagging/data/latex_text_50_results.csv', index_col=0)
+
+def top_latex_tokens(keyword):
+        """
+        This function accepts a keyword and returns a pandas dataframe of the
+        LaTeX tokens extracted for questions containing that keyword sorted
+        by the number of times the tokens occur across all questions
+        @params
+            - keyword: python string for particular keyword
+        """
+        # Get index of row containing keyword
+        index = tag_df[ tag_df['keyword'] == keyword].index[0]
+
+        # Extract LaTeX tokens and counts
+        latex_dict = ast.literal_eval( tag_df.iloc[index]['latex_tokens'] )
+
+        # Put information in pandas DataFrame for quick sorting
+        tokens = latex_dict.keys()
+        count = []
+        for token in tokens:
+            count.append( latex_dict[token] )
+
+        token_df = pd.DataFrame({'token' : tokens, 'count' :  count})
+        token_df = token_df.sort('count', ascending=False).head(20)
+        
+        print token_df['token'].values
+        return None
+
 
 def random_question():
     """
@@ -46,6 +74,10 @@ def get_about_page(index):
     AJAX request method to serve pages of the about presentation
     """
     template = 'about%s.html' % index
+    
+    if index == '4':
+        top_latex_tokens('calculus')
+        
     return render_template(template);
     
 
