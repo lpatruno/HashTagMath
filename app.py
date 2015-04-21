48c2d09cb346166c2445f34f62016154c6988eee
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-21 14:00:25 -0400

    bio

--- app.py
+++ app.py
@@ -107,6 +107,7 @@ def biography():
         
     
 if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # For testing purposes
+    #app.run(host='0.0.0.0', debug=True)
