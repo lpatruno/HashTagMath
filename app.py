88dd4436650a9cfcdc1db67b107c5d348acd8448
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-21 13:33:49 -0400

    bio page

--- app.py
+++ app.py
@@ -107,5 +107,6 @@ def about_me():
         
     
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', debug=True)
