d213186f235078daecfdf4bc9c08c2b7f255ba49
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-21 11:47:53 -0400

    deploy

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify
 import pandas as pd
 import random
 import ast
+import os
 
 app = Flask(__name__)
 
@@ -99,4 +100,6 @@ def about():
         
     
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(host='0.0.0.0', debug=True)
