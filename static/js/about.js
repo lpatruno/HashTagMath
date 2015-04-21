fce81d85cffe6093776bd9c38ecd2005d25a2adc
Luigi Patruno <luigi.patruno.91@gmail.com>
2015-04-21 11:40:52 -0400

    ready to deploy

---
+++ static/js/about.js
@@ -0,0 +1,54 @@
+function get_about_page( index ){
+	
+	var url = '/about/' + String(index);
+	
+	$.get(url, function(data){
+		
+		$('#container').html(data);
+		
+	});
+	
+	// Render all LaTeX content
+	if (index == 0){
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	}
+}
+
+function get_tokens( keyword ){
+	
+	var url = '/latex/' + keyword;
+	
+	$.get(url, function(data){
+		tokens = data['tokens'];
+		token_string = '';
+		
+		for (var i=0; i<tokens.length; i++){
+			token_string += "<span style='padding-left:15px;'>" + tokens[i] + "</span>";
+		}
+		$('#tokens').html(token_string);
+		
+	});
+}
+
+$( document ).ready(function() {
+	
+	i = 0;
+	
+	$('#next').click(function(){
+		if (i<5){
+			i = i + 1;
+			get_about_page(i);
+		}
+	});
+	
+	$('#back').click(function(){
+		if (i>0){
+			i = i - 1;
+			get_about_page(i);
+		}
+	});
+	
+	get_about_page(i);
+	
+});
+
---
+++ static/js/index.js
@@ -0,0 +1,38 @@
+function get_question(){
+	
+	$.get('/getQuestion', function(data){
+		var question = data['question'];
+		var keywords = data['keywords'];
+		var model_2 = data['model_2'];
+		
+		var keyword_string = '';
+		var model_2_string ='';
+		
+		for(var i=0; i<keywords.length; i++){
+			keyword_string += ' <code>' + keywords[i] + '</code> ';
+		}
+		keyword_string += '<br>'
+		
+		for(var i=0; i<model_2.length; i++){
+			model_2_string += ' <code>' + model_2[i] + '</code> ';
+		}
+		model_2_string += '<br>'
+		
+		
+		$('#questionText').html(question);
+		$('#keywords').html(keyword_string);
+		$('#prediction').html(model_2_string);
+		
+		// Render all LaTeX content
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	});
+}
+
+$( document ).ready(function() {
+    
+	get_question();
+	
+	$('#newQuestion').click(function(){get_question();})
+	
+});
+
